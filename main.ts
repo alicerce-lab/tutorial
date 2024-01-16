0a326bbddd4ff500f38013b6f107ef7f046a777a
BrenoPinheir0 <156707223+BrenoPinheir0@users.noreply.github.com>
2024-01-16 17:25:25 +0000

    fatorail

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
 console.log("ola mundo")
+
+function calcularfatorial(numero: number): number {
+    if (numero < 0) {
+        throw new Error("O fatorial é indefinido para números negativos.");
+    }
+
+    if (numero === 0 || numero === 1) {
+        return 1;
+    } else {
+        return numero * calcularfatorial(numero - 1);
+    }
+}
+
+const numeroparacalcular = 5;
+const resultadofatorial = calcularfatorial(numeroparacalcular);
+
+console.log(calcularfatorial(5))
+
+
+
+
