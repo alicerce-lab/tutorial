9f3c0ea24a195fef83145bdb7d3fa14c782aa923
Gabriel Melo Correia <gmfc@users.noreply.github.com>
2024-01-18 14:26:59 +0000

    add iterative version

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
 console.log("ola mundo")
 
+function fact(num:number) {
+    if (num < 0) throw new Error("O fatorial é indefinido para números negativos.");
+    let resultado = 1
+    while(num > 0) {
+        resultado = resultado * num
+        num--
+    }
+    return resultado
+}
+
 function calcularfatorial(numero: number): number {
     if (numero < 0) {
         throw new Error("O fatorial é indefinido para números negativos.");
@@ -16,6 +26,7 @@ const numeroparacalcular = 5;
 const resultadofatorial = calcularfatorial(numeroparacalcular);
 
 console.log(calcularfatorial(5))
+console.log(fact(5))
 
 
 
